e1e755050d4b7dd574eeb08498f7fad8fd49b73c
Kajetan Rzepecki <k@spartan.works>
2018-01-23 23:34:09 +0100

    Turned board contents into a table.

--- frontend/src/app/containers/board/board.tsx
+++ frontend/src/app/containers/board/board.tsx
@@ -14,14 +14,31 @@ export class Board extends preact.Component<Props, any> {
     console.log("Board clicked!");
   }
 
+  iota(n: number): Array<number> {
+    return Array.from(new Array(n).keys());
+  }
+
   render () {
+    const dimX = this.props.store.boardDim.x;
     return (
       <div className={styles.boardWrapper}>
-        { this.props.store.board.map((field) => (
-            // TODO Update the board contents.
-            <Field contents={field} onClick={this.onClick} />
+        <table>
+        { this.iota(this.props.store.boardDim.y).map((y) => (
+            <tr>
+              { this.iota(dimX).map((x) => (
+                // TODO Update the board contents.
+                (
+                  <td>
+                    <Field contents={this.props.store.board[y * dimX + x]}
+                           onClick={this.onClick} />
+                  </td>
+                )
+              ))
+              }
+              </tr>
           ))
         }
+        </table>
       </div>
     );
   }
