5661368f28c46e2010e741b342b49554987ecd76
Kajetan Rzepecki <k@spartan.works>
2018-01-24 03:42:25 +0100

    Added some assets to the app.

--- frontend/src/app/components/field/field.tsx
+++ frontend/src/app/components/field/field.tsx
@@ -8,8 +8,29 @@ interface Props {
   isSelected: boolean;
 }
 
+function typeToClass(type: string): string {
+  switch (type) {
+  case "empty":
+    return "";
+  case "gravel":
+    return styles.gravel;
+  case "boulder":
+    return styles.boulder;
+  case "wh_entrance":
+    return styles.whEntrance;
+  case "wh_exit":
+    return styles.whExit;
+  case "start":
+    return styles.start;
+  case "end":
+    return styles.end;
+  default:
+    return styles.unknown;
+  }
+}
+
 export const Field = (props: Props) => (
-    <div className={styles.fieldWrapper + " " + (props.isSelected ? styles.selected : "")} onClick={props.onClick}>
-    { props.contents.type }
+  <div className={styles.fieldWrapper + " " + (props.isSelected ? styles.selected : "")} onClick={props.onClick}>
+    <div className={styles.fieldImage + " " + typeToClass(props.contents.type)} />
   </div>
 );
