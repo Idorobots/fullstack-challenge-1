4403e5f6a400878af049bdc53e8fe8996a56b92d
Kajetan Rzepecki <k@spartan.works>
2018-01-24 18:16:14 +0100

    Fixed tests in the backend.

--- backend/src/test/scala/oc/MainSpec.scala
+++ backend/src/test/scala/oc/MainSpec.scala
@@ -12,7 +12,7 @@ class MainSpec extends FunSpec with Matchers with ScalatestRouteTest {
     it("should respond to status/ requests") {
       implicit val system: ActorSystem = ActorSystem("test")
       implicit val materializer: ActorMaterializer = ActorMaterializer()
-      implicit val routes = Route.seal(Main.routes)
+      implicit val routes = Route.seal(Main.routes(Solver()))
 
       Get(s"/api/status") ~> routes ~> check {
         status shouldBe StatusCodes.OK
@@ -22,7 +22,7 @@ class MainSpec extends FunSpec with Matchers with ScalatestRouteTest {
     it("should respond with 404 to unknown routes") {
       implicit val system: ActorSystem = ActorSystem("test")
       implicit val materializer: ActorMaterializer = ActorMaterializer()
-      implicit val routes = Route.seal(Main.routes)
+      implicit val routes = Route.seal(Main.routes(Solver()))
 
       Get(s"/api/some/other/route") ~> routes ~> check {
         status shouldBe StatusCodes.NotFound
