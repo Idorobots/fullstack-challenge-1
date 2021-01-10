cc489218dae3c986b1f01a45e94da2ac10d2dac8
Kajetan Rzepecki <k@spartan.works>
2021-01-10 18:45:31 +0100

    Oops, forgot to update the BE tests.

--- backend/src/test/scala/oc/MainSpec.scala
+++ backend/src/test/scala/oc/MainSpec.scala
@@ -4,15 +4,15 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.stream.ActorMaterializer
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest._
+import funspec._
+import matchers._
 
-class MainSpec extends FunSpec with Matchers with ScalatestRouteTest {
+class MainSpec extends AnyFunSpec with should.Matchers with ScalatestRouteTest {
   describe("MainSpec") {
     it("should respond to status/ requests") {
       implicit val system: ActorSystem = ActorSystem("test")
-      implicit val materializer: ActorMaterializer = ActorMaterializer()
-      implicit val routes = Route.seal(Main.routes(Solver()))
+      implicit val routes: Route = Route.seal(Main.routes(Solver()))
 
       Get(s"/api/status") ~> routes ~> check {
         status shouldBe StatusCodes.OK
@@ -21,8 +21,7 @@ class MainSpec extends FunSpec with Matchers with ScalatestRouteTest {
 
     it("should respond with 404 to unknown routes") {
       implicit val system: ActorSystem = ActorSystem("test")
-      implicit val materializer: ActorMaterializer = ActorMaterializer()
-      implicit val routes = Route.seal(Main.routes(Solver()))
+      implicit val routes: Route = Route.seal(Main.routes(Solver()))
 
       Get(s"/api/some/other/route") ~> routes ~> check {
         status shouldBe StatusCodes.NotFound
